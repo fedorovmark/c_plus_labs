1f91b5f1705853fcd2c0c232ad76c5f497239598
fedorovmark <fedorov.ma@phystech.edu>
2023-11-23 05:47:14 +0300

    realised integr with some problems:)

--- INTEGR/test_mr.cpp
+++ INTEGR/test_mr.cpp
@@ -1,5 +1,7 @@
 #include <array>
 #include <type_traits>
+#include <cmath>
+#include <iostream>
 
 template <typename RealType, unsigned int N> 
 struct nodes;
@@ -18,8 +20,8 @@ struct nodes<RealType, 2> {
 
 template <typename RealType> 
 struct nodes<RealType, 3> {
-    static constexpr std::array<RealType, 3> p{-0.7745966692, 0, 0.7745966692};
-    static constexpr std::array<RealType, 3> w{0.5555555556, 0.8888888889, 0.5555555556};
+    std::array<RealType, 3> p{-0.7745966692, 0, 0.7745966692};
+    std::array<RealType, 3> w{0.5555555556, 0.8888888889, 0.5555555556};
 };
 
 template <typename RealType> 
@@ -45,27 +47,46 @@ struct ArgumentGetter<R(Arg)> {
 template<typename T>
 using Dif = decltype(std::declval<T>() - std::declval<T>());
 
+
 /* Функция производит интегрирование на одном отрезке */
-template<typename Callable, typename RealType,  std::size_t N>
+template<typename Callable, typename RealType, std::size_t N>
 decltype(auto) integrate(   
     const Callable& func,  // Интегрируемая функция
     const typename ArgumentGetter<Callable>::Argument& start,  
     const typename ArgumentGetter<Callable>::Argument& end,
     const nodes<RealType, N>& node
                         ) {
-                            double gauss_sum = 0.;
-                            for (auto i = 0; i < N; i++) {
-                                gauss_sum += func(node.p[i]) * node.w[i];
-                            }
-                            
-                            return gauss_sum;
-                        }
+                            Dif<RealType> polusum = (end + start) / 2;
+                            Dif<RealType> poluraz = (end - start) / 2;
+                            RealType gauss_sum = 0.;
 
-/* Функция производит интегрирование, разбивая отрезок на подотрезки длиной не более dx */
-template<typename Callable, std::size_t N>
-decltype(auto) integrate(   
-    const Callable& func,  // Интегрируемая функция
-    const typename ArgumentGetter<Callable>::Argument& start,  // начало отрезка
-    const typename ArgumentGetter<Callable>::Argument& end,  // конец отрезка
-    const Dif<typename ArgumentGetter<Callable>::Argument>& dx  // Длина подотрезка
-                        );
+                            for (auto i = 0; i < N; i++) {
+                                gauss_sum += poluraz * func(polusum + poluraz * node.p[i]) * node.w[i];
+                            }
+
+                            return gauss_sum;
+                        };
+
+
+
+// /* Функция производит интегрирование, разбивая отрезок на подотрезки длиной не более dx */
+// template<typename Callable, std::size_t N>
+// decltype(auto) integrate(   
+//     const Callable& func,  // Интегрируемая функция
+//     const typename ArgumentGetter<Callable>::Argument& start,  // начало отрезка
+//     const typename ArgumentGetter<Callable>::Argument& end,  // конец отрезка
+//     const Dif<typename ArgumentGetter<Callable>::Argument>& dx  // Длина подотрезка
+//                         );
+
+double func(double x) {
+    return std::sin(x);
+}
+
+int main() {
+    double res;
+    double st = 0.;
+    double end = 1;
+    nodes<double, 3> node;
+    res = integrate(func, st, end, node);
+    std::cout << res << std::endl;
+}
